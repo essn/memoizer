47cedd6aec6bea1161f2369cd5a39a17bed218c0
Jesse Pledger <jessepledger@gmail.com>
2021-11-06 18:29:54 -0400

    Test that the resources is protected against multiple hits

--- src/lib/caches/singletonCache.spec.ts
+++ src/lib/caches/singletonCache.spec.ts
@@ -18,3 +18,26 @@ test('singletonCache only ever stores one value', (t) => {
   const memoizedValue2Take2 = memoizedAdder(1, 3);
   t.deepEqual(memoizedValue2Take2, 4);
 });
+
+test('singletonCache protects memoized resource from multiple calls', async (t) => {
+  // eslint-disable-next-line functional/no-let
+  let counter = 0;
+
+  const protekted = (a, b) => {
+    counter += 1;
+
+    return a + b;
+  };
+
+  const memoizedProtekted = memoizer()(protekted);
+
+  const aDecentAmountOfArguments = new Array(10).fill(1);
+
+  await Promise.all(
+    aDecentAmountOfArguments.map((i) => {
+      return new Promise((resolve) => resolve(memoizedProtekted(i, i)));
+    })
+  );
+
+  t.deepEqual(counter, 1);
+});
